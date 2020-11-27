58c4b562f23b33a830be714190768af0604e8407
Artem <sobminsk@gmail.com>
2020-11-27 14:46:40 +0300

    Include sde_drm.h

--- fod/FodExtension.cpp
+++ fod/FodExtension.cpp
@@ -15,14 +15,16 @@
  */
 
 #include <compositionengine/FodExtension.h>
+#include <drm/sde_drm.h>
 
 uint32_t getFodZOrder(uint32_t z, bool touched) {
     if (!touched) {
-        z |= 0x1000000;
+        z |= FOD_PRESSED_LAYER_ZORDER;
     }
     return z;
 }
 
 uint64_t getFodUsageBits(uint64_t usageBits, bool) {
+    (void) touched;
     return usageBits;
 }
