a16e4a0772bd852a3ccf9dea3f6b3cb7dc2c0061
Artem <sobminsk@gmail.com>
2020-11-28 00:31:18 +0300

    Update FodExtension.cpp

--- fod/FodExtension.cpp
+++ fod/FodExtension.cpp
@@ -17,7 +17,7 @@
 #include <compositionengine/FodExtension.h>
 
 uint32_t getFodZOrder(uint32_t z, bool touched) {
-    if (touched) {
+    if (!touched) {
         z |= 0x1000000;
     }
     return z;
